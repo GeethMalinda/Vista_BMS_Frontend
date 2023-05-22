77a8b3bc7ee7501c059abb1efeb19cf47f23cabb
Geeth <kdgmwijesekara22@gmail.com>
2023-05-22 07:38:26 +0530

    improvements

--- src/App.js
+++ src/App.js
@@ -7,30 +7,20 @@ import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import Auth from "./components/auth/Auth";
 import Home from "./components/user/Home/Home";
 import BookDetails from './components/user/PostDetails/BookDetails';
-import { ThemeEditorProvider } from "@hypertheme-editor/chakra-ui";
-import { ChakraProvider } from "@chakra-ui/react";
-import theme from "./theme/theme";
 
 const App = () => {
     return (
-        <ChakraProvider theme={theme}>
-            <React.StrictMode>
-                <ThemeEditorProvider>
-                    <BrowserRouter>
-                        <Container maxWidth="xl">
-                            <Navbar />
-                            <Switch>
-                                <Route path="/" exact component={() => <Redirect to="/auth" />} />
-                                <Route path="/home" exact component={Home} />
-                                <Route path="/auth" exact component={SignUp} />
-                                <Route path="/posts/:id" exact component={BookDetails} />
-                            </Switch>
-                        </Container>
-                    </BrowserRouter>
-                </ThemeEditorProvider>
-            </React.StrictMode>
-        </ChakraProvider>
-
+        <BrowserRouter>
+            <Container maxWidth="xl">
+                <Navbar />
+                <Switch>
+                    <Route path="/" exact component={() => <Redirect to="/auth" />} />
+                    <Route path="/home" exact component={Home} />
+                    <Route path="/auth" exact component={SignUp} />
+                    <Route path="/posts/:id" exact component={BookDetails} />
+                </Switch>
+            </Container>
+        </BrowserRouter>
 
     );
 };
